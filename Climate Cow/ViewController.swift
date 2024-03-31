847541d884a4c3a9d945f931b0fdced3b0df985d
CocoTheCoder <diya.v@icloud.com>
2024-03-31 10:16:38 -0400

    Initial Commit

---
+++ Climate Cow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climate Cow
+//
+//  Created by Diya on 3/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
